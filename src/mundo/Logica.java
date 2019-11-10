2785f93a6b45294e1361a9730505dc54c283d06b
Joshua <ccquicenol@uqvirtual.edu.co>
2019-11-10 17:58:08 -0500

    Hace FNC
    puede convertir a FNC, falta documentar y otros metodos

--- src/mundo/Logica.java
+++ src/mundo/Logica.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class Logica {
 
+	public static final String NEGACION = "¬";
+	public static final String CONJUNCION = "∧";
+	public static final String DISYUNCION = "∨";
+	public static final String CONDICIONAL = "→";
+	public static final String EQUIVALENCIA = "↔";
 	/**
 	 * Metodo para verificar si es un operador
 	 * @param caracter
@@ -15,6 +20,135 @@ public class Logica {
 		}
 		return false;
 	}
+	/*********************************************************************************
+	                                    FNC
+	 *********************************************************************************/
+	/**
+     * dada una formula la lleva a FNC
+     */
+    private String fnc(String entrada) {
+        int contador1 = 0;
+        String derecha = "", izquierda = "", tmp1 = "", iz, der;
+        for (int i = 0; i < entrada.length(); i++) {
+            if (entrada.charAt(i) == ')')
+                contador1--;
+            else if (entrada.charAt(i) == '(')
+                contador1++;
+            if (contador1 == 0) {
+                izquierda = iz = entrada.substring(1, i);
+                derecha = der = entrada.substring(i + 3, entrada.length() - 1);
+
+                String tmpIzquierda = "", tmpDerecho = "", f = (entrada.charAt(i + 1) + "");
+                int hf = 1;
+                if (f.equals("(") || f.equals(")"))
+                    hf = 2;
+                if ((entrada.charAt(i + hf) + "").equals(DISYUNCION)) {
+                    String opDerecha = getOperadorPrincipal(derecha), opIzquierda = getOperadorPrincipal(izquierda);
+                    if ((opDerecha.equals("") || opDerecha.equals(DISYUNCION)) && (opIzquierda.equals(CONJUNCION))) {
+                        tmpIzquierda = getLados(izquierda).get(0);
+                        tmpDerecho = getLados(izquierda).get(1);
+
+                        izquierda = "(" + derecha + ")" + DISYUNCION + "(" + tmpIzquierda + ")";
+                        derecha = "(" + derecha + ")" + DISYUNCION + "(" + tmpDerecho + ")";
+
+
+                    } else if ((opIzquierda.equals("") || opIzquierda.equals(DISYUNCION)) && (opDerecha.equals(CONJUNCION))) {
+                        tmpIzquierda = getLados(derecha).get(0);
+                        tmpDerecho = getLados(derecha).get(1);
+                        derecha = "(" + izquierda + ")" + DISYUNCION + "(" + tmpDerecho + ")";
+                        izquierda = "(" + izquierda + ")" + DISYUNCION + "(" + tmpIzquierda + ")";
+
+                    } else if ((opIzquierda.equals(CONJUNCION)) && (opDerecha.equals(CONJUNCION))) {
+                        tmpIzquierda = getLados(derecha).get(0);
+                        tmpDerecho = getLados(derecha).get(1);
+                        derecha = "(" + izquierda + ")" + DISYUNCION + "(" + tmpDerecho + ")";
+                        izquierda = "(" + izquierda + ")" + DISYUNCION + "(" + tmpIzquierda + ")";
+
+                    }
+                    if (getOperadorPrincipal(izquierda).equals(DISYUNCION) && izquierda.contains(CONJUNCION))
+                        izquierda = fnc(izquierda);
+
+                    if (getOperadorPrincipal(derecha).equals(DISYUNCION) && derecha.contains(CONJUNCION))
+                        derecha = fnc(derecha);
+
+                    if (iz.equals(izquierda) && der.equals(derecha)) {
+                        tmp1 = entrada;
+                        break;
+                    } else {
+                        if (iz.equals(izquierda) || der.equals(derecha))
+                            tmp1 = fnc("(" + izquierda + ")" + DISYUNCION + "(" + derecha + ")");
+                        else
+                            tmp1 = "(" + izquierda + ")" + CONJUNCION + "(" + derecha + ")";
+                    }
+                } else {
+                    if (derecha.contains(CONJUNCION))
+                        derecha = fnc(derecha);
+                    if (izquierda.contains(CONJUNCION))
+                        izquierda = fnc(izquierda);
+
+                    tmp1 = "(" + izquierda + ")" + CONJUNCION + "(" + derecha + ")";
+
+
+                }
+
+                break;
+            }
+
+        }
+
+        System.out.println(tmp1);
+        return tmp1;
+    }
+    
+    /**
+     * retorna los lados de una ecuacion
+     */
+    private ArrayList<String> getLados(String entrada) {
+
+        if (entrada.charAt(0) == NEGACION.charAt(0))
+            entrada = entrada.substring(2, entrada.length() - 1);
+        int contador1 = 0;
+        String derecha = "", izquierda = "";
+        ArrayList<String> retorno = new ArrayList<>();
+        for (int i = 0; i < entrada.length(); i++) {
+            if (entrada.charAt(i) == ')')
+                contador1--;
+            else if (entrada.charAt(i) == '(')
+                contador1++;
+            if (contador1 == 0) {
+                derecha = entrada.substring(1, i);
+                izquierda = entrada.substring(i + 3, entrada.length() - 1);
+                retorno.add(izquierda);
+                retorno.add(derecha);
+                break;
+            }
+        }
+        return retorno;
+    }
+    
+    /**
+     * obtiene el operador principal de una ecuacion
+     */
+    private String getOperadorPrincipal(String entrada) {
+        String t = "";
+        if (!entrada.contains(DISYUNCION) && !entrada.contains(CONJUNCION))
+            return "";
+        int contador1 = 0;
+        for (int i = 0; i < entrada.length() - 1; i++) {
+            if (entrada.charAt(i) == ')')
+                contador1--;
+            else if (entrada.charAt(i) == '(')
+                contador1++;
+            if (contador1 == 0) {
+                t = entrada.charAt(i + 1) + "";
+                break;
+            }
+        }
+        return t;
+    }
+	
+	
+	
 	
 	/********************************************************************************
 	                                  Resolucion
